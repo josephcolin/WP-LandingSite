d42b48a24ec87940de95bd27da6019d2598bf643
unknown <joseph@dependable-dumpsters.com>
2020-07-30 20:15:51 -0400

    commented out unused JS script.

--- js/scripts.js
+++ js/scripts.js
@@ -9,26 +9,26 @@ function toggleMenu() {
     
 
 
-//  Email Validation
+//  Email Validation Didn't function; abandoned.
 
-function emailValidation () {
-  var form = document.getElementById("form");
-  var email = document.getElementById("email").value;
-  var text = document.getElementById("text");
-  var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+// function emailValidation () {
+//   var form = document.getElementById("form");
+//   var email = document.getElementById("email").value;
+//   var text = document.getElementById("text");
+//   var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-  if (email.match(pattern)) {
-    form.classList.add("valid");
-    form.classList.remove("invalid");
-    text.innerHTML = "Your email is valid.";
-    text.style.color = "#00ff00";
+//   if (email.match(pattern)) {
+//     form.classList.add("valid");
+//     form.classList.remove("invalid");
+//     text.innerHTML = "Your email is valid.";
+//     text.style.color = "#00ff00";
 
-  }else {
-    form.classList.remove("valid");
-    form.classList.add("invalid");
-    text.innerHTML = "Please enter a valid email.";
-    text.style.color = "#ff0000";
+//   }else {
+//     form.classList.remove("valid");
+//     form.classList.add("invalid");
+//     text.innerHTML = "Please enter a valid email.";
+//     text.style.color = "#ff0000";
     
-  }
+//   }
 
-}
+// }
